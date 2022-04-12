1b3c1035774a0a153d8629381380363abc10cb9e
y-a-s-h-j-a-i-n <98091626+y-a-s-h-j-a-i-n@users.noreply.github.com>
2022-04-12 20:06:40 +0530

    Add files via upload

---
+++ 15_03_2022/pthread_once.c
@@ -0,0 +1,7 @@
+// #include<pthread.h>
+#include<stdio.h>
+int main()
+{
+    printf("sjdbkdf");
+    return 0;
+}
---
+++ 15_03_2022/pthread_priority.cpp
@@ -0,0 +1,6 @@
+#include<pthread.h>
+#include<stdio.h>
+void *my(void *i)
+{
+    printf("\n )
+}
